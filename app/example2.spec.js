7d9a3467d84e2f0060a2295e5db62343656b6f63
jguerola <jguerola@grm-e.com>
2018-02-06 16:37:56 +0100

    initial setup karma

---
+++ app/example2.spec.js
@@ -0,0 +1,6 @@
+describe('Token utility', function() {
+    it('token 4 = 4', function() {
+      // An intentionally failing test. No code within expect() will never equal 4.
+      expect(4).toEqual(4);
+    });
+  });
