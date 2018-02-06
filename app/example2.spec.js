45c5a684220158b0044da49ff9986ad812b5ef6b
jguerola <jguerola@grm-e.com>
2018-02-06 17:43:53 +0100

    main.js

--- app/example2.spec.js
+++ app/example2.spec.js
@@ -1,6 +1,7 @@
 describe('Token utility', function() {
     it('token 4 = 4', function() {
       // An intentionally failing test. No code within expect() will never equal 4.
+      debugger;
       expect(4).toEqual(4);
     });
   });
